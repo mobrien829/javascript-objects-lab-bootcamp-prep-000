a2d729d4d41f7fa4fd900c17e8b2518917c4040b
Michael O%27Brien <mobrien@colgate.edu>
2018-05-02 19:27:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
-  delete Object.assign(object, { [key]: value })
+  return Object.assign(object, { [key]: value })
 }
